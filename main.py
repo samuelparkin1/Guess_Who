486525fe42e241597bc82684d341a1c634121ba2
samuelparkin1 <samuelparkin1@gmail.com>
2021-07-11 22:02:52 +1000

    User input now asks and store user information

--- main.py
+++ main.py
@@ -1,33 +1,45 @@
 import os
 
-name = "Sam"
-eyes = {'john':"green", 'sam': "green", 'tom': "blue", 'jim': "brown"}
-eye_colours = ("Amber","Blue","Brown", "Gray", "Green", "Hazel")
-hair = {}
-hair_colours =("Black", "Brown", "Blonde", "White", "Grey", "Red", "Pink", "Purple", "Blue", "Green", "Yellow")
-glasses ={}
-glasses_options = ("Yes", "No") 
+name = []
 
-def colour_input(category, feature_list, feature_colour ): 
+player_genders= {}
+genders=("Male", "Female", "Other")
+
+player_eye_colours = {'john':"green", 'sam': "green", 'tom': "blue", 'jim': "brown"}
+eye_colours = ("Amber","Blue","Brown", "Gray", "Green", "Hazel")
+
+player_hair_colours = {}
+hair_colours =("Black", "Brown", "Blonde", "White", "Grey", "Red", "Pink", "Purple", "Blue", "Green", "Yellow")
+
+players_glasses ={}
+glasses_options = ("Yes", "No")
+
+players_bald ={}
+bald_options=("Yes","No")
+
+players_facial_hair ={}
+facial_hair_options=("Yes","No")
+
+def player_name():
     while True:
-        userinput = input(f"please enter the colour of your {category}:").capitalize()
+        userinput = input("What is your full name?").capitalize()
         os.system('cls||clear')
-        if userinput not in feature_colour:
-            print ("\nWhich of these colours is the closest?")
-            print (', '.join(feature_colour)+"\n")
+        if " " not in userinput:
+            print ("\nDont forget your last name?")            
         else:
-            feature_list[name] = userinput
+            global name 
+            name = userinput
             os.system('cls||clear')
             break
 
-def yes_no_input(question, feature_list, feature_colour ): 
+def user_input(category, error_message, feature_list, feature_options ): 
     while True:
-        userinput = input(question).capitalize()
+        userinput = input(f" {category}:").capitalize()
         os.system('cls||clear')
-        if userinput not in feature_colour:
-            print ("\nPlease specify Yes or No")
-            #print (', '.join(feature_colour)+"\n")
-        else:
+        if userinput not in feature_options:
+            print (error_message)
+            print (', '.join(feature_options)+"\n")
+        else:            
             feature_list[name] = userinput
             os.system('cls||clear')
             break
@@ -35,15 +47,32 @@ def yes_no_input(question, feature_list, feature_colour ):
 #clear terminal window
 os.system('cls||clear')
 
+#get user name
+player_name()
+
+#user to enter gender 
+user_input("Please enter your gender: ", "\nPlease select from the following options: ", player_genders, genders)
+
 #user to put in their eye colour. 
-colour_input("eyes", eyes, eye_colours)
+user_input("Please enter your eye colour: ", "\nWhich eye colour is closest to yours: ", player_eye_colours, eye_colours)
 
 #user to put in their hair colour. 
-colour_input("hair colour", hair, hair_colours)
+user_input("Please enter the your hair colour: ", "\nWhich hair colour is closest to yours: ",player_hair_colours, hair_colours)
 
 #ask user if they wear glasses.
-yes_no_input("do you were glasses: ", glasses, glasses_options)
+user_input("Do you wear glasses: ","\nPlease specify", players_glasses, glasses_options)
 
-print (eyes)
-print (hair)
-print (glasses)
+#ask user if they're bald.
+user_input("Are you follicly challenged: ","\nPlease specify", players_bald, bald_options)
+
+#ask user if they're bald.
+user_input("Do you have facial hair: ","\nPlease specify", players_facial_hair, facial_hair_options)
+
+print (name)
+print (player_genders)
+print (player_eye_colours)
+print (player_hair_colours)
+print (players_glasses)
+print (players_bald)
+print (players_facial_hair)
+
