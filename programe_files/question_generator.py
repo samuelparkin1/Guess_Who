71289d9e42bc208bc46044de81f7bae7ff2ebd79
samuelparkin1 <samuelparkin1@gmail.com>
2021-07-14 11:10:22 +1000

    The Question Generator now asks all categoies. Fixed the bug that occured when removing a yes or no question once it was answered

--- programe_files/question_generator.py
+++ programe_files/question_generator.py
@@ -1,18 +1,23 @@
 import random
 from typing import Coroutine
-from user_data import player_genders, player_eye_colours, player_hair_colours
+from user_data import players, player_genders, player_eye_colours, player_hair_colours, player_glasses, players_bald, players_facial_hair
 from filter import x,test_yes, test_no
 temporary_players =[]
 temporary_eye_colour =[]
-temporary_hair_colour=["Black", "Brown", "Blonde", "White", "Grey", "Red", "Pink", "Purple", "Blue", "Green", "Yellow"]
-temporary_gender=["Male", "Female", "Other"]
-temporary_bald = ["Yes", "No"]
-temporary_glasses = ["Yes", "No"]
-temporary_facial_hair = ["Yes", "No"]
+temporary_hair_colour=[]
+temporary_gender=[]
+temporary_bald = []
+temporary_glasses = []
+temporary_facial_hair = []
 answer_options= ("Yes", "No")
 question_countdown = 10
 
-# This function is to generate a tempory lists. 
+# This function is to generate a tempory lists.
+def temporary_player_list(data, temp_list):
+    for i in data:
+        if i not in temp_list:
+            temp_list.append(i)
+
 # It takes fasical catgoryies and makes a list of the differnt options within then.  
 def temporary_list(data, temp_list):
     for i in data.values():
@@ -28,14 +33,16 @@ def question_generator(question, function):
                 print ("Please type Yes or No")
             elif answer == "Yes":
                 global question_list, question_countdown
-                question_list.remove(function)
+                if function in question_list:
+                    question_list.remove(function)
                 question_countdown -= 1
                 break
             else:
                 question_countdown -= 1
                 break
             
-
+# This function will choose a random eye colour from the temporary_eye_colour list.
+# Once the eye colour is asked it will remove it from the list so not to asked again  
 def eye_question():
     try:
         random_eye_colour = random.choice(temporary_eye_colour)
@@ -46,6 +53,8 @@ def eye_question():
         question_list.remove(eye_question)
         print("hmm.. I can't think of any other eye colours")
 
+# This function will choose a random hair colour from the temporary_hair_colour list.
+# Once the hair colour is asked it will remove it from the list so not to asked again.
 def hair_question():
     try:
         random_hair_colour = random.choice(temporary_hair_colour)
@@ -56,6 +65,8 @@ def hair_question():
         question_list.remove(hair_question)
         print("hmm.. I can't think of any other hair colour")
 
+# This function will choose a random gender from the temporary_gender list.
+# Once the gender has been asked it will remove it from the list so not to asked again.
 def gender_question():
     try:
         random_gender = random.choice(temporary_gender)
@@ -66,33 +77,47 @@ def gender_question():
         question_list.remove(gender_question)
         print("hmm.. I can't think of any other genders")
 
+# This function will choose a random bald option from the temporary_bald list.
+# Once the option has been asked it will remove it from the list so not to asked again.
 def bald_question():
     try:
         question = (f"Are you bald? ")
         question_generator(question, bald_question)
-        question_list.remove(bald_question)
+        if  bald_question in question_list:
+            question_list.remove(bald_question)
     except IndexError:
         question_list.remove(bald_question)
         print("hmm.. I can't remember if you had hair or not")
 
+# This function will choose a random glasses option from the temporary_glasses list.
+# Because this is only a Yes or No option, it will remove the function from the question list
+# so not to asked again.
 def glasses_question():
     try:
         question = (f"Do you wear glasses? ")
         question_generator(question, glasses_question)
-        question_list.remove(glasses_question)
+        if glasses_question in question_list:
+            question_list.remove(glasses_question)
     except IndexError:
         question_list.remove(glasses_question)
         print("hmm.. I can't remember if you wear glasses or not")
 
+# This function will choose a random glasses option from the temporary_glasses list.
+# Because this is only a Yes or No option, it will remove the function from the question list
+# so not to asked again.
 def facial_hair_question():
     try:
         question = (f"Do you have facial hair? ")
         question_generator(question, facial_hair_question)
-        question_list.remove(facial_hair_question)
+        if facial_hair_question in question_list:
+            question_list.remove(facial_hair_question)
     except IndexError:
         question_list.remove(facial_hair_question)
         print("hmm.. I can't remember if you had facial hair")
 
+
+# Gererate a temporary list of players.
+temporary_player_list(players,temporary_players)
 
 # Gererate a temporary list of possible eye colours.  
 temporary_list(player_eye_colours,temporary_eye_colour)
@@ -103,10 +128,27 @@ temporary_list(player_hair_colours,temporary_hair_colour)
 # Gererate a temporary list of passible hair colours. 
 temporary_list(player_genders,temporary_gender)
 
+# Gererate a temporary list of passible glasses options 
+temporary_list(player_glasses,temporary_glasses)
 
+# Gererate a temporary list of passible bald options. 
+temporary_list(players_bald,temporary_bald)
+
+# Gererate a temporary list of passible facial hair options.
+temporary_list(players_facial_hair,temporary_facial_hair)
+
+#This is a list of the question fuctions that input into the question generator.
 question_list = [eye_question, hair_question, gender_question, bald_question, glasses_question, facial_hair_question]
-#select a question at random 
 
+print (temporary_players)
+print (temporary_eye_colour)
+print (temporary_hair_colour)
+print (temporary_gender)
+print (temporary_bald)
+print (temporary_glasses)
+print (temporary_facial_hair)
+
+# This pick a question function from the question list at random
 for i in range(100):
     try:
         random.choice(question_list)()  
