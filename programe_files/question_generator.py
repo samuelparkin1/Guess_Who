2be9b20e2da62a1fd73c36479ce01efcd06e47a7
samuelparkin1 <samuelparkin1@gmail.com>
2021-07-14 15:48:47 +1000

    The program now asks 3 question followed by an attempt of a name.

--- programe_files/question_generator.py
+++ programe_files/question_generator.py
@@ -1,31 +1,55 @@
 import random
-from typing import Coroutine
+from typing import Coroutine, Counter
 from user_data import players, player_genders, player_eye_colours, player_hair_colours, player_glasses, players_bald, players_facial_hair
 #from filter import x,test_yes, test_no
 temporary_players_options =[]
-temporary_eye_colour_options =[]
+temporay_eye_colour_options =[]
 temporary_hair_colour_options=[]
 temporary_gender_options=[]
 temporary_bald_options = []
 temporary_glasses_options = []
 temporary_facial_hair_options = []
 answer_options= ("Yes", "No")
-question_countdown = 10
+feature_guesses = 0
 
-# This function is to generate a tempory lists.
+# This function is to generate a temporary lists.
 def temporary_player_list(data, temp_list):
     for i in data:
         if i not in temp_list:
             temp_list.append(i)
 
-# It takes fasical catgoryies and makes a list of the differnt options within then.  
+# It takes the players category and makes a list of the different options. 
+# It ensure there is no double up in the list.  
 def temporary_list(data, temp_list):
     for i in data.values():
         if i not in temp_list:
             temp_list.append(i)
+      
+# This function is to generate random NAME for the user to answer.
+def name_question ():
+    try:
+        random_name = random.choice(temporary_players_options)
+        temporary_players_options.remove(random_name)
+        global feature_guesses
+        feature_guesses = 0      
+        while True:            
+            answer = input (f"Is your name {random_name} ? ").capitalize()
+            if answer not in answer_options:
+                print ("Please type Yes or No")
+            elif answer == "Yes":
+                print (f"Oh hey there {random_name}")
+                return True
+                
+            else:
+                print ("hmm let me keep trying")
+                return False
 
+    except IndexError:                
+        print("hmm.. I can't think of your name")
+        
 
 # This function is to generate random question for the user to answer.
+# ONLY WORKS FOR DICTIONARIES
 def question_generator(question_to_user, question_function, user_data, option):
         while True:
             answer = input (question_to_user).capitalize()
@@ -38,11 +62,11 @@ def question_generator(question_to_user, question_function, user_data, option):
                     if names in temporary_players_options:
                         temporary_players_options.remove(names)
                 #Because the player does match the criteria the question shouldn't be asked again.
-                global question_list, question_countdown
+                global question_list, feature_guesses
                 if question_function in question_list:
                     question_list.remove(question_function)
                 print (temporary_players_options)  
-                question_countdown -= 1
+                feature_guesses += 1
                 break
             else:
                 #This loop will remove any players name that DO match to uses answer
@@ -51,15 +75,15 @@ def question_generator(question_to_user, question_function, user_data, option):
                     if names in temporary_players_options:
                         temporary_players_options.remove(names)
                 print (temporary_players_options)
-                question_countdown -= 1
+                feature_guesses += 1
                 break
             
 # This function will choose a random eye colour from the temporary_eye_colour list.
 # Once the eye colour is asked it will remove it from the list so not to asked again  
 def eye_question():
     try:
-        random_eye_colour = random.choice(temporary_eye_colour_options)
-        temporary_eye_colour_options.remove(random_eye_colour)
+        random_eye_colour = random.choice(temporay_eye_colour_options)
+        temporay_eye_colour_options.remove(random_eye_colour)
         question = (f"Do you have {random_eye_colour} eyes? ")
         question_generator(question, eye_question, player_eye_colours, random_eye_colour)
     except IndexError:
@@ -128,12 +152,11 @@ def facial_hair_question():
         question_list.remove(facial_hair_question)
         print("hmm.. I can't remember if you had facial hair")
 
-
 # Gererate a temporary list of players.
 temporary_player_list(players,temporary_players_options)
 
 # Gererate a temporary list of possible eye colours.  
-temporary_list(player_eye_colours,temporary_eye_colour_options)
+temporary_list(player_eye_colours,temporay_eye_colour_options)
 
 # Gererate a temporary list of passible hair colours. 
 temporary_list(player_hair_colours,temporary_hair_colour_options)
@@ -150,7 +173,7 @@ temporary_list(players_bald,temporary_bald_options)
 # Gererate a temporary list of passible facial hair options.
 temporary_list(players_facial_hair,temporary_facial_hair_options)
 
-#This is a list of the question fuctions that input into the question generator.
+#This is a list of the question functions that input into the question generator.
 question_list = [eye_question, hair_question, gender_question, bald_question, glasses_question, facial_hair_question]
 
 # print (temporary_players_options)
@@ -160,16 +183,24 @@ question_list = [eye_question, hair_question, gender_question, bald_question, gl
 # print (temporary_bald_options)
 # print (temporary_glasses_options)
 # print (temporary_facial_hair_options)
+print (int(feature_guesses))
 
-# This pick a question function from the question list at random
+# This picks a question function from the question list at random
 while True:
     try:
-        if len(question_list) >1:
+        if int(feature_guesses) < 3 and len (question_list) > 1 and len(temporary_players_options) > 1:
             random.choice(question_list)()
+            print (int(feature_guesses))
+                
         else:
-            print ("I must of miss heard one of you're answers.. I'm not sure who you are")
-            break
+            if len(temporary_players_options) > 1:
+                name_question()
+            else:
+                print("Seems like you slipped my detection")
+                break                     
+ 
     except IndexError:
         print ("I've run out of question....")
         break
-
+    
+print ("loop complete")
