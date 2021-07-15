50e43471fe3a3a8c3c0d3175fcb7869b955fc395
samuelparkin1 <samuelparkin1@gmail.com>
2021-07-15 12:06:21 +1000

    Have made some changes on whothe program is presented to the the user. has got more instruction on who the game is played.

--- programe_files/question_generator.py
+++ programe_files/question_generator.py
@@ -1,16 +1,31 @@
 import random
-from typing import Coroutine, Counter
-from user_data import players, player_genders, player_eye_colours, player_hair_colours, player_glasses, players_bald, players_facial_hair
-#from filter import x,test_yes, test_no
-temporary_players_options =[]
-temporay_eye_colour_options =[]
-temporary_hair_colour_options=[]
-temporary_gender_options=[]
-temporary_bald_options = []
-temporary_glasses_options = []
-temporary_facial_hair_options = []
-answer_options= ("Yes", "No")
-feature_guesses = 0
+import os
+from user_data import *
+
+def player_name():
+    while True:
+        userinput = input("What is your full name? ").capitalize()
+        os.system('cls||clear')
+        if " " not in userinput:
+            print ("Don't forget your last name.\n")            
+        else:
+            global name 
+            name = userinput
+            players.append(name)
+            os.system('cls||clear')
+            break
+
+def user_input(category, error_message, feature_list, feature_options ): 
+    while True:
+        userinput = input(f"{category}").capitalize()
+        os.system('cls||clear')
+        if userinput not in feature_options:
+            print (error_message)
+            print (', '.join(feature_options)+"\n")
+        else:            
+            feature_list[name] = userinput
+            os.system('cls||clear')
+            break
 
 # This function is to generate a temporary lists.
 def temporary_player_list(data, temp_list):
@@ -37,10 +52,12 @@ def name_question ():
             if answer not in answer_options:
                 print ("Please type Yes or No")
             elif answer == "Yes":
+                os.system('cls||clear')
                 print (f"Oh hey there {random_name}")
                 return True
                 
             else:
+                os.system('cls||clear')
                 print ("hmm let me keep trying")
                 return False
 
@@ -64,8 +81,7 @@ def question_generator(question_to_user, question_function, user_data, option):
                 #Because the player does match the criteria the question shouldn't be asked again.
                 global question_list, feature_guesses
                 if question_function in question_list:
-                    question_list.remove(question_function)
-                print (temporary_players_options)  
+                    question_list.remove(question_function) 
                 feature_guesses += 1
                 break
             else:
@@ -145,7 +161,7 @@ def glasses_question():
 def facial_hair_question():
     try:
         question = (f"Do you have facial hair? ")
-        question_generator(question, facial_hair_question, player_hair_colours, "Yes")
+        question_generator(question, facial_hair_question, players_facial_hair, "Yes")
         if facial_hair_question in question_list:
             question_list.remove(facial_hair_question)
     except IndexError:
@@ -153,65 +169,146 @@ def facial_hair_question():
         print("hmm.. I can't remember if you had facial hair")
 
 
-    #Ask the user if they're ready to play.
-    play_agin = "Yes"
-    player_ready = input ("Ready to play guess who?")
-    while player_ready == "Yes" and play_agin == "Yes":
-        
-        feature_guesses = 0
+run_game = "Yes"
+while run_game == 'Yes':
+    os.system('cls||clear')
+    print("""   _____                      __     __           ___  
+  / ____|                     \ \   / /          |__ \ 
+ | |  __ _   _  ___  ___ ___   \ \_/ /__  _   _     ) |
+ | | |_ | | | |/ _ \/ __/ __|   \   / _ \| | | |   / / 
+ | |__| | |_| |  __/\__ \__ \    | | (_) | |_| |  |_|  
+  \_____|\__,_|\___||___/___/    |_|\___/ \__,_|  (_)  """)
 
-        # Gererate a temporary list of players.
-        temporary_player_list(players,temporary_players_options)
+    print ("""
+                Created by Sam Parkin""")
+    userinput = input("""
+                Would you like to play?
+                Please enter Yes / Exit.\n
+                        """).capitalize()
 
-        # Gererate a temporary list of possible eye colours.  
-        temporary_list(player_eye_colours,temporay_eye_colour_options)
+    if userinput not in ["Yes" , "Exit"]:
+        os.system('cls||clear')
+        print ("Please enter Yes or Exit" )
 
-        # Gererate a temporary list of passible hair colours. 
-        temporary_list(player_hair_colours,temporary_hair_colour_options)
+    elif userinput == "Yes":
+        run_game = userinput          
+        new_game = userinput
+        os.system('cls||clear')
 
-        # Gererate a temporary list of passible hair colours. 
-        temporary_list(player_genders,temporary_gender_options)
+        while run_game == "Yes" and new_game == "Yes":
+            #clear terminal window
+            os.system('cls||clear')
 
-        # Gererate a temporary list of passible glasses options 
-        temporary_list(player_glasses,temporary_glasses_options)
+            #Welcome message
+            print ("Hi and welcome to Guess Who!")
+            print ("Before we move on, I just need to know a little about you.\n")
 
-        # Gererate a temporary list of passible bald options. 
-        temporary_list(players_bald,temporary_bald_options)
+            #get user name
+            player_name()
 
-        # Gererate a temporary list of passible facial hair options.
-        temporary_list(players_facial_hair,temporary_facial_hair_options)
+            #user to enter gender 
+            user_input("What is your gender? ", "Please select from the following options: ", player_genders, genders)
 
-        #This is a list of the question functions that input into the question generator.
-        question_list = [eye_question, hair_question, gender_question, bald_question, glasses_question, facial_hair_question]
+            #user to put in their eye colour. 
+            user_input("What colour eyes do you have? ", "Which eye colour is closest to yours: ", player_eye_colours, eye_colours)
 
-        # print (temporary_players_options)
-        # print (temporary_eye_colour_options)
-        # print (temporary_hair_colour_options)
-        # print (temporary_gender_options)
-        # print (temporary_bald_options)
-        # print (temporary_glasses_options)
-        # print (temporary_facial_hair_options)
-        print (int(feature_guesses))
+            #user to put in their hair colour. 
+            user_input("What colour hair do you have? ", "Which hair colour is closest to yours: ",player_hair_colours, hair_colours)
 
-        # This loops starts asking the user random questions.
-        
-        while True:
-            try:
-                if int(feature_guesses) < 3 and len (question_list) > 1 and len(temporary_players_options) > 2:
-                    random.choice(question_list)()
-                    print (int(feature_guesses))
-                        
-                else:
-                    if len(temporary_players_options) > 0:
-                        if name_question() == True:
-                            break                
+            #ask user if they wear glasses.
+            user_input("Do you wear glasses? ","Please enter", player_glasses, glasses_options)
+
+            #ask user if they're bald.
+            user_input("Would you say you're bald? ","Please enter", players_bald, bald_options)
+
+            #ask user if they're bald.
+            user_input("Lastly, Do you have facial hair? ","Please enter", players_facial_hair, facial_hair_options)
+                #Ask the user if they're ready to play.
+
+            while True:
+                print ("Thanks for that!\n")
+                print ("My friend, Inspector Bot, considers themselves as a real Sherlock Holmes and believe they can find anybody!")
+                print ("Inspector Bot is going to ask some random question to guess who you are.")
+                userinput = input("\nAre you ready to play Guess You against Inspector Bot or would you like to add another player?\n\nYes, Add or Exit: ").capitalize()
+                os.system('cls||clear')
+                if userinput not in ["Yes" , "Add", "Exit"]:
+                    print ("Please enter Yes or No" )
+                elif userinput == "Yes":
+                    play_again = userinput            
+                    os.system('cls||clear')
+                    break
+                elif userinput == "Add":
+                    play_again = ""
+                    break
+                elif userinput == "Exit":
+                    new_game = ""
+                    break
+
+            while play_again == "Yes":
+                     
+                feature_guesses = 0
+
+                # Gererate a temporary list of players.
+                temporary_player_list(players,temporary_players_options)
+
+                # Gererate a temporary list of possible eye colours.  
+                temporary_list(player_eye_colours,temporay_eye_colour_options)
+
+                # Gererate a temporary list of passible hair colours. 
+                temporary_list(player_hair_colours,temporary_hair_colour_options)
+
+                # Gererate a temporary list of passible hair colours. 
+                temporary_list(player_genders,temporary_gender_options)
+
+                # Gererate a temporary list of passible glasses options 
+                temporary_list(player_glasses,temporary_glasses_options)
+
+                # Gererate a temporary list of passible bald options. 
+                temporary_list(players_bald,temporary_bald_options)
+
+                # Gererate a temporary list of passible facial hair options.
+                temporary_list(players_facial_hair,temporary_facial_hair_options)
+
+                #This is a list of the question functions that input into the question generator.
+                question_list = [eye_question, hair_question, gender_question, bald_question, glasses_question, facial_hair_question]
+              
+                while True:
+                    try:
+                        if int(feature_guesses) < 3 and len (question_list) > 1 and len(temporary_players_options) > 2:
+                            random.choice(question_list)()
+                            os.system('cls||clear')
+                                
+                        else:
+                            if len(temporary_players_options) > 0:
+                                if name_question() == True:
+                                    break                
+                            else:
+                                print("Seems like you slipped my detection")
+                                break                     
+                
+                    except IndexError:
+                        print ("I've run out of question....")
+                        break
+
+                while True:
+                    userinput = input("Do you want to play again?\nYes or Exit: ").capitalize()
+                    os.system('cls||clear')
+                    if userinput not in ["Yes" , "Exit"]:
+                        print ("Please enter Yes or Exit\n")
+                    elif userinput == "Yes":
+                        play_agin = userinput
+                        os.system('cls||clear')
+                        break   
                     else:
-                        print("Seems like you slipped my detection")
-                        break                     
-        
-            except IndexError:
-                print ("I've run out of question....")
-                break
+                        play_again = userinput            
+                        new_game = userinput
+                        os.system('cls||clear')
+                        break               
+    else:
+        run_game = userinput          
+        new_game = userinput
+        os.system('cls||clear')
+        break
 
-        play_agin = input ("Play again?")      
+     
 
