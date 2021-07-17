156e9c93a00649dd4bc4ae94d53d858219503450
samuelparkin1 <samuelparkin1@gmail.com>
2021-07-17 13:11:30 +1000

    have made changes to the bash script. now checks if the python file is in teh  correct location before sappemting to start.

--- guess_you.sh
+++ guess_you.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
+
+# Checks to see if the user has Python 3 installed 
 if ! [[ -x "$(command -v python3)" ]]
+#If the user does NOT have python 3 installed it will display an error message 
+#   and direct them to were they can install it.  
 then
   echo 'Error: 
     This program runs on Python, but it looks like Python is not installed.
     To install Python, check out https://installpython3.com/' >&2
   exit 1
-
+# if the user does have Python three 
 else
+  # Checks to see if the user has a programe files
+  if [ ! -e "programe_files/guesSs_you.py" ]
+  then
+    echo "Unable to find files to start the programme. Please refer to help.txt documentation for troubleshooting." >&2
+    exit 1
+  else
     python3 programe_files/guess_you.py $1
+  fi
 fi
 
  
